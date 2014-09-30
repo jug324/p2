3eccdc3e3a3b7e5371ee5d895e2e28d300944b99
Justin Gilman <justingilman@g.harvard.edu>
2014-09-29 21:22:11 -0400

    Initial styling

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
 <head>
 <meta charset="UTF-8">
 <title>Random Password Generator</title>
+<link rel='stylesheet' href="p2styles.css"/>
 <?php require ("banks.php") ?>
 <?php require ("logic.php") ?>
 </head>
 
-<body>
+<body id="container">
 <h1>Random Password Generator</h1>
 <h3>Use the tool below to generate a random password comprised of multiple words</h3>
+<div class="password"><p>Password:</p><p class="passwordbox"><?php foreach ($password as $value)
+    echo $value;?><?php echo $symbol.$number ?></p></div>
 <form action="index.php" method="GET">
 <p>Number of words to use:
 <select name="wordcount" >
@@ -24,10 +27,8 @@
 <p>Should it include a symbol?   Yes<input type="checkbox" name="symbol" value="include"></p>
 <div><input type="submit" value="Create Password"></div>
 </form>
-
-<p><?php foreach ($password as $value)
-    echo $value;?><?php echo $symbol.$number ?></p>
-
-<div><p>This password generator is inspired by the comic XKCD. I hope it serves you well.</p><img src="http://imgs.xkcd.com/comics/password_strength.png" /><p><a href="http://xkcd.com/936/">XKCD comic by Randall Monroe</a></p></div>
+<br>
+<br>
+<div><p>This password generator is inspired by the comic XKCD. I hope it serves you well.</p><img src="http://imgs.xkcd.com/comics/password_strength.png" class="xkcd" /><p><a href="http://xkcd.com/936/">XKCD comic by Randall Munroe</a></p></div>
 </body>
 </html>
